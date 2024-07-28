9be79cdfef58c2cc37e7f851e433589ead41fdd5
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-28 15:12:59 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,43 +1,16 @@
-const request = require('request');
-const pick = require('lodash').pick;
 const shouldCompress = require('./shouldCompress');
 const compress = require('./compress');
-const bypass = require('./bypass');
-const copyHeaders = require('./copyHeaders');
 
-function (req, res) {
-  request.get(
-    req.params.url,
-    {
-      headers: {
-        ...pick(req.headers, ['dnt']),
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-        'x-forwarded-for': req.ip,
-      },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) {
-        return bypass(req, res, buffer);
-      }
+function handler(req, res) {
+  // Logic to generate content or fetch from a data source
+  // Replace this placeholder with your actual content generation logic
+  const content = generateContent(req);
 
-      copyHeaders(origin, res);
-      res.setHeader('content-encoding', 'identity');
-      req.params.originType = origin.headers['content-type'] || '';
-      req.params.originSize = buffer.length;
-
-      if (shouldCompress(req)) {
-        compress(req, res, buffer);
-      } else {
-        bypass(req, res, buffer);
-      }
-    }
-  );
+  if (shouldCompress(req)) {
+    compress(req, res, content);
+  } else {
+    res.send(content);
+  }
 }
 
-module.exports = "";
+module.exports = handler;
