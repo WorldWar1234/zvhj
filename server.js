58d990c0f1cd1dd83a04bbb76a38cdf0461c8e85
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-28 15:39:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 'use strict';
-const app = require('express')();
+const Fastify = require('fastify');
 const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 
-const PORT = process.env.PORT || 8080;
+const app = Fastify({ logger: true });
 
-app.enable('trust proxy');
 app.get('/', authenticate, params, proxy);
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get('/favicon.ico', (_, reply) => reply.code(204).send());
+
+app.listen({ port: process.env.PORT || 8080 }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`server listening on ${address}`);
+});
