e8b5591537dac97d4daa8b7ad510f8bb5143da53
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-29 11:56:49 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -2,8 +2,7 @@ const shouldCompress = require('./shouldCompress');
 const compress = require('./compress');
 const bypass = require('./bypass');
 
-// Define the handler function outside
-function handler(request, reply) {
+function handler(request, reply, buffer) {
   // Logic to generate content or fetch from a data source
   // Replace this placeholder with your actual content generation logic
 
@@ -14,4 +13,4 @@ function handler(request, reply) {
   }
 }
 
-module.exports = handler;
+module.exports = { handler, shouldCompress, compress, bypass };
